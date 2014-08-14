583435a4fa9f4b2194ae1f3d2b470b40a7b86c8d
luffyj <luffy.ja@gmail.com>
2014-08-14 17:41:44 +0800

    PUT 403?

---
+++ src/main/webapp/wresources/login.js
@@ -0,0 +1,11 @@
+/* 
+ * 实现ajax login 避免chrome等浏览器的bug影响登录
+ */
+var wzqrserver = wzqrserver || {};
+
+wzqrserver.login = function(form){
+    console.log($('form'));
+//    $.post('login',{},function(data, textStatus, jqXHR){});
+};
+
+
