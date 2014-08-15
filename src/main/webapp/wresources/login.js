5ec7f8f523bb5bad259278a52dbbfca57c30d7c0
luffyj <luffy.ja@gmail.com>
2014-08-15 09:05:17 +0800

    final

--- src/main/webapp/wresources/login.js
+++ src/main/webapp/wresources/login.js
@@ -13,26 +13,35 @@ wzqrserver.login = function(form) {
         jcaptcha: $('input[name=jcaptcha]').val(),
         ajax: true
     }, function(data, textStatus, jqXHR) {
-        console.log(jqXHR,textStatus);
-        console.log(jqXHR.getResponseHeader('Location'));
-        if (data.status === 200) {
+//        console.log(jqXHR,textStatus);
+//        console.log(jqXHR.getResponseHeader('Location'));
+        if (jqXHR.status === 200) {
+            $.ajax('queryUser',{complete:function(){
+                var href = window.location.href;
+                var index = href.lastIndexOf('/');
+                var newHref = href.substring(0,index+1);
+                window.location.href = newHref;
+            }})
 //            window.location.reload();
         }
     }).error(function(data, textStatus) {
-        console.log(data);
+//        console.log(data);
         if (data.status === 410) {
             $('img[title=点击刷新]').click();
             $('#errorMsg').html('验证码错误');
+            return;
         }
 
         if (data.status === 500) {
             $('img[title=点击刷新]').click();
             $('#errorMsg').html('系统繁忙，请重试');
+            return;
         }
 
         if (data.status === 401) {
             $('img[title=点击刷新]').click();
             $('#errorMsg').html('用户名或者密码错误');
+            return;
         }
 
         $('img[title=点击刷新]').click();
